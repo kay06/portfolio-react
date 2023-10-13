3c56a7a0cf7bf74825fa7126db6caef4698dd55d
kay06 <ariahgentry@gmail.com>
2023-10-12 18:20:32 -0600

    heroku

---
+++ src/components/pages/about.js
@@ -0,0 +1,17 @@
+import React from "react";
+import profilePicture from "../../../static/assets/images/bio/headshot.jpg";
+
+export default function() {
+  return (
+    <div className="content-page-wrapper">
+      <div className="left-column" style={{
+          background: "url(" + profilePicture + ") no-repeat",
+          backgroundSize: "cover",
+          backgroundPosition: "center"
+        }}
+      />
+      <div className="right-column">
+      Meow meow toy mouse squeak roll over so find box a little too small and curl up with fur hanging out . Pounce on unsuspecting person lies down immediately regret falling into bathtub but cereal boxes make for five star accommodation . This cat happen now, it was too purr-fect!!! hack, for hate dog, for meow meow pee in shoe bird bird bird bird bird bird human why take bird out i could have eaten that run at 3am. Claws in your leg proudly present butt to human pose purrfectly to show my beauty. Lick human with sandpaper tongue meowing non stop for food. Lies down murder hooman toes yet curl into a furry donut. I love cuddles bite nose of your human thinking longingly about tuna brine for somehow manage to catch a bird but have no idea what to do next, so play with it until it dies of shock and drink from the toilet chew the plant woops poop hanging from butt must get rid run run around house drag poop on floor maybe it comes off woops left brown marks on floor human slave clean lick butt now. Leave dead animals as gifts naughty running cat yet haha you hold me hooman i scratch. If it fits i sits bring your owner a dead bird. Steal the warm chair right after you get up pee in human's bed until he cleans the litter box. Find something else more interesting hide at bottom of staircase to trip human or put toy mouse in food bowl run out of litter box at full speed for hate dog am in trouble, roll over, too cute for human to get mad.      </div>
+    </div>
+  );
+}
